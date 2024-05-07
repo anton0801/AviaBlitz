c70d370cdc0f93a63ded147694fe50475755aeaa
Anton <danilovanton724@gmail.com>
2024-05-07 16:48:33 +0200

    Initial Commit

---
+++ Avia Blitz/Avia_BlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avia_BlitzApp.swift
+//  Avia Blitz
+//
+//  Created by Anton on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Avia_BlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
